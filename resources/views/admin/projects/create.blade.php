155fb312d8597cef226c10c6233fed7d5a01d3f4
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-11-02 19:34:19 +0100

    fix create: preview cover

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -19,75 +19,101 @@
                 </ul>
             </div>
         @endif
-
-        <form class="row g-3 mt-4" action="{{ route('admin.projects.store')}}" method="POST" enctype="multipart/form-data">
-            {{-- token da inserire per farlo leggere a laravel (questioni di sicurezza) --}}
-            @csrf
+        <div class="row">
+            <div class="col-8">
+                <form class="row g-3 mt-4" action="{{ route('admin.projects.store')}}" method="POST" enctype="multipart/form-data">
+                    {{-- token da inserire per farlo leggere a laravel (questioni di sicurezza) --}}
+                    @csrf
+                    <div class="col-4">
+                        <label class="d-block" for="name">Nome</label>
+                        <input type="text" id="name" name="name" class="form-control">
+                    </div>
+                    
+                    <div class="col-4">
+                        <label class="d-block" for="type_id">Tipo</label>
+                        <select class="form-select" aria-label="Default select example" name="type_id" id="type_id">
+                            @foreach ($types as $type)
+                                <option value="{{$type->id}}">{{$type->label}}</option>
+                            @endforeach
+                          </select>
+                    </div>
+        
+                    <div class="col-4">
+                        <label class="d-block" for="link">Link</label>
+                        <input type="text" id="link" name="link" class="form-control">
+                    </div>
+        
+                    <div class="col-6">
+        
+                        <label class="form-label">Tecnologia:</label>
+        
+                        <div class="form-check @error('tecnologies') is-invalid @enderror p-0">
+                            @foreach ($tecnologies as $tecnology)
+        
+                                <span class="me-3">
+                                    <input
+                                    type="checkbox"
+                                    id="tecnology-{{ $tecnology->id }}"
+                                    value="{{ $tecnology->id }}"
+                                    name="tecnologies[]"
+                                    class="form-check-control"
+                                    {{-- @if (in_array($tecnology->id, old('tecnologies', $project_tecnology ?? []))) checked @endif --}}
+                                    >
+                                    <label for="tecnology-{{ $tecnology->id }}">
+                                    {{ $tecnology->name }}
+                                    </label>
+                                </span>
+                                
+                            @endforeach
+                        </div>
+        
+                    </div>
+        
+                    <div class="col-6">
+                        <label class="d-block" for="cover_image">Cover</label>
+                        <input class="form-control" type="file" id="cover_image" name="cover_image" value="{{old('cover_image')}}">
+                    </div>
+        
+                    <div class="col-3">
+                        <label class="d-block" for="slug">Slug</label>
+                        <input type="text" id="slug" name="slug" class="form-control">
+                    </div>
+                    <div class="col-12">
+                        <label class="d-block" for="description">Descrizione</label>
+                        <input type="text" id="description" name="description" class="form-control">
+                    </div>
+                    <div class="col-6">
+                        <button class="btn btn-primary">SALVA</button>
+                    
+                    </div>
+                </form>
+            </div>
             <div class="col-4">
-                <label class="d-block" for="name">Nome</label>
-                <input type="text" id="name" name="name" class="form-control">
+                <img src="" alt="" class="image-fluid" id="cover_image_preview">
             </div>
-            
-            <div class="col-4">
-                <label class="d-block" for="type_id">Tipo</label>
-                <select class="form-select" aria-label="Default select example" name="type_id" id="type_id">
-                    @foreach ($types as $type)
-                        <option value="{{$type->id}}">{{$type->label}}</option>
-                    @endforeach
-                  </select>
-            </div>
-
-            <div class="col-4">
-                <label class="d-block" for="link">Link</label>
-                <input type="text" id="link" name="link" class="form-control">
-            </div>
-
-            <div class="col-6">
-
-                <label class="form-label">Tecnologia:</label>
-
-                <div class="form-check @error('tecnologies') is-invalid @enderror p-0">
-                    @foreach ($tecnologies as $tecnology)
-
-                        <span class="me-3">
-                            <input
-                            type="checkbox"
-                            id="tecnology-{{ $tecnology->id }}"
-                            value="{{ $tecnology->id }}"
-                            name="tecnologies[]"
-                            class="form-check-control"
-                            {{-- @if (in_array($tecnology->id, old('tecnologies', $project_tecnology ?? []))) checked @endif --}}
-                            >
-                            <label for="tecnology-{{ $tecnology->id }}">
-                            {{ $tecnology->name }}
-                            </label>
-                        </span>
-                        
-                    @endforeach
-                </div>
-
-            </div>
-
-            <div class="col-6">
-                <label class="d-block" for="cover_image">Cover</label>
-                <input class="form-control" type="file" id="cover_image" name="cover_image" value="{{old('cover_image')}}">
-            </div>
-            
-            <div class="col-3">
-                <label class="d-block" for="slug">Slug</label>
-                <input type="text" id="slug" name="slug" class="form-control">
-            </div>
-            <div class="col-12">
-                <label class="d-block" for="description">Descrizione</label>
-                <input type="text" id="description" name="description" class="form-control">
-            </div>
-            <div class="col-6">
-                <button class="btn btn-primary">SALVA</button>
-            
-            </div>
-        </form>
+        </div>
+        
         
         
     </div>
     
+@endsection
+
+@section('scripts')
+<script type="text/javascript"> 
+    // mi aggancio alla input della sezione "Cover"
+    const inputFileElement = document.getElementById('cover_image');
+    // mi aggancio alla img dove si trova l'immagine caricata/da caricare
+    const coverImagePreview = document.getElementById('cover_image_preview');
+
+    // quando l'input della sezione Cover, cambia allora...
+    inputFileElement.addEventListener('change', function(){
+        //prendi la sezione files di questo progetto
+        const [file] = this.files;
+        
+        //tramite l'ibreria "URL.createObjectURL( )" crea un url temporaneo dell'immagine che si trova in "files"
+        coverImagePreview.src = URL.createObjectURL(file);
+    })
+</script>
+
 @endsection
